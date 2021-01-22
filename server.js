e1e202180d6175ede2d463e6ca2919c6a0bb5af6
Mohammad Moien <mnmoienian@gmail.com>
2021-01-22 18:57:35 +0330

    add POST and GET event for user data

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const User = require("./models/User");
 const port = 8000;
 const app = express();
-mongoose.connect("mongodb://localhost/userData");
+mongoose.connect("mongodb://localhost:27017/userData");
 
 app.use(bodyParser.json());
 
@@ -14,14 +14,43 @@ app.listen(port, () => {
 
 // CREATE
 app.post("/users", (req, res) => {
-  // User.create()
+  User.create(
+    {
+      name: req.body.newData.name,
+      email: req.body.newData.email,
+      password: req.body.newData.password,
+    },
+    (err, data) => {
+      if (err) {
+        res.json({ success: false, message: err });
+      } else if (!data) {
+        res.json({ success: false, message: "Not Found" });
+      } else {
+        res.json({ success: true, data: data });
+      }
+    }
+  );
 });
 
 app
   .route("/users/:id")
   // READ
   .get((req, res) => {
-    // User.findById()
+    User.findById(req.params.id, (err, data) => {
+      if (err) {
+        res.json({ success: false, message: err });
+      } else if (!data) {
+        res.json({
+          success: false,
+          message: "Not Found",
+        });
+      } else {
+        res.json({
+          success: true,
+          data: data,
+        });
+      }
+    });
   })
   // UPDATE
   .put((req, res) => {
