c4d495dbc7ebe60e83d4a528c3d345be9b8878c3
Mohammad Moien <mnmoienian@gmail.com>
2021-01-22 19:20:18 +0330

    refactor code for using sendResponse function

--- server.js
+++ server.js
@@ -12,24 +12,21 @@ app.listen(port, () => {
   console.log(`server is listening on port:${port}`);
 });
 
+function sendResponse(res, err, data) {
+  if (err) {
+    res.json({ success: false, message: err });
+  } else if (!data) {
+    res.json({ success: false, message: "Not Found" });
+  } else {
+    res.json({ success: true, data: data });
+  }
+}
+
 // CREATE
 app.post("/users", (req, res) => {
-  User.create(
-    {
-      name: req.body.newData.name,
-      email: req.body.newData.email,
-      password: req.body.newData.password,
-    },
-    (err, data) => {
-      if (err) {
-        res.json({ success: false, message: err });
-      } else if (!data) {
-        res.json({ success: false, message: "Not Found" });
-      } else {
-        res.json({ success: true, data: data });
-      }
-    }
-  );
+  User.create({ ...req.body.newData }, (err, data) => {
+    sendResponse(res, err, data);
+  });
 });
 
 app
@@ -37,41 +34,19 @@ app
   // READ
   .get((req, res) => {
     User.findById(req.params.id, (err, data) => {
-      if (err) {
-        res.json({ success: false, message: err });
-      } else if (!data) {
-        res.json({
-          success: false,
-          message: "Not Found",
-        });
-      } else {
-        res.json({
-          success: true,
-          data: data,
-        });
-      }
+      sendResponse(res, err, data);
     });
   })
   // UPDATE
   .put((req, res) => {
     User.findByIdAndUpdate(
       req.params.id,
-      {
-        name: req.body.newData.name,
-        email: req.body.newData.email,
-        password: req.body.newData.password,
-      },
+      { ...req.body.newData },
       {
         new: true,
       },
       (err, data) => {
-        if (err) {
-          res.json({ success: false, message: err });
-        } else if (!data) {
-          res.json({ success: false, message: "Not Found" });
-        } else {
-          res.json({ success: true, data: data });
-        }
+        sendResponse(res, err, data);
       }
     );
   })
